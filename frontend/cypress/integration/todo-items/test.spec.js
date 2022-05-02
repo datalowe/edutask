06e0e6ca3a56685c5bcbd5d6a61725911a9e85c2
Janni Karlsson <jannikarlsson@gmail.com>
2022-05-02 14:07:03 +0100

    Restored disappeared file

--- frontend/cypress/integration/todo-items/test.spec.js
+++ frontend/cypress/integration/todo-items/test.spec.js
@@ -21,19 +21,7 @@ describe('Test adding task to video list', () => {
     })
   })
 
-  // // Fungerande test som lägger till en item och kontrollerar dess text, borde vara rimlig nivå för R8UC1 #1
-  // it('adds new task to list', () => {
-  //   let text = "Take out the trash"
-  //   cy.get('input[placeholder="Add a new todo item"]')
-  //   .type(text, { force: true })
-  //   cy.contains('input', 'Add').parent()
-  //     .submit()
-  //   cy.get('li')
-  //     .should('contain.text', text)
-  // })
-
   // R8UC1 #1
-  // Denna funkar oftast men failar ibland, enligt Julians hintar ska det bero på något bakomliggande fel i system så det är inget fel på koden utan ska vara så typ? Svårt att förstå.
   it('adds new task to list', () => {
     let text = "Take out the trash"
     cy.get('li').its('length').then((origLen) => {
@@ -48,15 +36,10 @@ describe('Test adding task to video list', () => {
   })
 
   // R8UC1 #2 part 1
-  // Denna kollar ändå att listan inte har blivit längre, vilket den har så testet failar men ska faila. 
-  // @janni ^ jag har nu ändrat så att det görs ett försök att klicka, som det står i specen. då går testet
-  // igenom som väntat. jag kanske missförstått hur testet ska göras dock.
   it('should not be possible to submit an empty string, meaning the task item list remains the same', () => {
     cy.get('input[placeholder="Add a new todo item"]').clear({ force: true })
     cy.get('ul').find('li').its('length').then(($len) => {
       cy.contains('input', 'Add').click({ force: true })
-      // @janni jag förstår inte nedan test, varför ska den kolla elementet innan första .todo-item?
-      // cy.get('.todo-item').prev().not('have.text', '')
       cy.get('li').its('length').should('be.eq', $len)
     })
   })
